7729b4ac62752ec722fede43db17130d8ce69ae4
AlexHearnNI <40436879+AlexHearnNI@users.noreply.github.com>
2018-07-18 12:19:28 -0500

    s/5164/5122/

--- src/niscope/examples/niscope_fetch_forever.py
+++ src/niscope/examples/niscope_fetch_forever.py
@@ -59,12 +59,12 @@ def main():
 
 
 def test_example():
-    options = {'simulate': True, 'driver_setup': {'Model': '5164', 'BoardType': 'PXIe', }, }
+    options = {'simulate': True, 'driver_setup': {'Model': '5122', 'BoardType': 'PXIe', }, }
     example('PXI1Slot2', options, 10, 1.0, 1000.0, 100)
 
 
 def test_main():
-    cmd_line = ['--option-string', 'Simulate=1, DriverSetup=Model:5164; BoardType:PXIe', ]
+    cmd_line = ['--option-string', 'Simulate=1, DriverSetup=Model:5122; BoardType:PXIe', ]
     _main(cmd_line)
 
 
